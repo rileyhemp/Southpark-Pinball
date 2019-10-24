5c2c748016c42d127000445e1ab933eb45fbd9d3
Riley Hemphill <rileyhemp@gmail.com>
2019-10-24 02:25:01 -0400

    Finished tutorial

--- src/js/main.js
+++ src/js/main.js
@@ -17,8 +17,12 @@ var config = {
 }
 
 var player
+var bombs
 var platforms
 var cursors
+var stars
+var score = 0
+var scoreText
 
 var game = new Phaser.Game(config);
 
@@ -72,6 +76,55 @@ function create ()
 
     this.physics.add.collider(player, platforms)
     cursors = this.input.keyboard.createCursorKeys()
+
+    stars = this.physics.add.group({
+        key: 'star',
+        repeat: 11,
+        setXY: { x:12, y: 0, stepX: 70 }
+    })
+
+    stars.children.iterate(function (child) {
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8))
+    })
+
+    this.physics.add.collider(stars, platforms)
+
+    this.physics.add.overlap(player, stars, collectStar, null, this)
+
+    function collectStar(player, star){
+        star.disableBody(true, true)
+        score += 10
+        scoreText.setText('Score: ' + score)
+
+        if (stars.countActive(true) === 0){
+            stars.children.iterate(function(child){
+                child.enableBody(true, child.x, 0, true, true)
+            })
+            //If player is on the left side, make set var to x coord on right side, visa versa.
+            var x = (player.x < 400) ? Phaser.Math.Between(400,800) :
+            Phaser.Math.Between(0,400)
+
+            //Make the bomb and add some props
+            var bomb = bombs.create(x, 16, 'bomb')
+            bomb.setBounce(1)
+            bomb.setCollideWorldBounds(true)
+            bomb.setVelocity(Phaser.Math.Between(-200, 200), 20)
+        }
+    }
+
+    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000'})
+
+    bombs = this.physics.add.group()
+    this.physics.add.collider(bombs, platforms)
+    this.physics.add.collider(player, bombs, hitBomb, null, this)
+
+    function hitBomb (player, bomb) {
+        this.physics.pause()
+        player.setTint(0xff0000)
+        player.anims.play('turn')
+        gameOver = true
+    }
+
 }
 
 function update ()
