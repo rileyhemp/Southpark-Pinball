45c03c9eca820fbe2a5a6d73ed773c5959715ba2
Riley Hemphill <rileyhemp@gmail.com>
2019-10-30 01:37:02 -0400

    Flippers started, semi functional

--- dist/js/game.js
+++ dist/js/game.js
@@ -39,7 +39,7 @@ var config = {
   }
 }; //Declare variables here
 
-var balls, spacebar, ball, bounds;
+var balls, spacebar, ball, bounds, leftFlipper, rightFlipper, leftBumper, rightBumper, leftBlock, leftPivot, leftJoint, constraint1, constraint2, leftStopper;
 var game = new Phaser.Game(config);
 
 function preload() {
@@ -50,6 +50,7 @@ function preload() {
 function create() {
   bounds = this.matter.world.setBounds(0, 0, 440, 800, 30, true, true, true, true);
   this.matter.world.setGravity(0, 1);
+  spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
   var Ball =
   /*#__PURE__*/
@@ -68,7 +69,6 @@ function create() {
       _get(_getPrototypeOf(Ball.prototype), "setCircle", _assertThisInitialized(_this)).call(_assertThisInitialized(_this), 13);
 
       scene.sys.displayList.add(_assertThisInitialized(_this));
-      console.log(scene);
       return _this;
     }
 
@@ -82,8 +82,82 @@ function create() {
     return Ball;
   }(Phaser.Physics.Matter.Image);
 
-  ball = new Ball(this, 430, 800, 'ball');
-  spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+  var Bumper =
+  /*#__PURE__*/
+  function (_Phaser$Physics$Matte2) {
+    _inherits(Bumper, _Phaser$Physics$Matte2);
+
+    function Bumper(scene, x, y, texture) {
+      var _this2;
+
+      _classCallCheck(this, Bumper);
+
+      _this2 = _possibleConstructorReturn(this, _getPrototypeOf(Bumper).call(this, scene.matter.world, x, y, texture));
+
+      _get(_getPrototypeOf(Bumper.prototype), "setStatic", _assertThisInitialized(_this2)).call(_assertThisInitialized(_this2), true);
+
+      scene.sys.displayList.add(_assertThisInitialized(_this2));
+      return _this2;
+    }
+
+    return Bumper;
+  }(Phaser.Physics.Matter.Sprite);
+
+  var Flipper =
+  /*#__PURE__*/
+  function (_Phaser$Physics$Matte3) {
+    _inherits(Flipper, _Phaser$Physics$Matte3);
+
+    function Flipper(scene, x, y, texture, orientation) {
+      var _this3;
+
+      _classCallCheck(this, Flipper);
+
+      _this3 = _possibleConstructorReturn(this, _getPrototypeOf(Flipper).call(this, scene.matter.world, x, y, texture)); // super.setStatic(true)
+
+      _get(_getPrototypeOf(Flipper.prototype), "setFriction", _assertThisInitialized(_this3)).call(_assertThisInitialized(_this3), .2);
+
+      scene.sys.displayList.add(_assertThisInitialized(_this3));
+
+      _this3.setScale(0.5, 1);
+
+      _this3.rotation = .4;
+      return _this3;
+    }
+
+    return Flipper;
+  }(Phaser.Physics.Matter.Image);
+
+  ball = new Ball(this, 215, 0, 'ball'); //Flipper mechanism
+
+  leftFlipper = new Flipper(this, 200, 700, 'rectA').setInteractive();
+  leftBlock = this.matter.add.image(175, 740, 'rectA', this, {
+    isStatic: true
+  });
+  Object.assign(leftBlock, {
+    scaleX: .5,
+    scaleY: .1,
+    rotation: .4
+  });
+  leftPivot = this.matter.add.image(120, 655, null, this, {
+    isStatic: true
+  });
+  leftPivot.setScale(.2);
+  constraint1 = this.matter.add.constraint(leftPivot, leftFlipper);
+  constraint1.stiffness = .7;
+  constraint1.length = 0;
+  constraint1.pointA = {
+    x: 10,
+    y: 10
+  };
+  constraint1.pointB = {
+    x: -65,
+    y: -40
+  };
+  leftStopper = this.matter.add.image(220, 600, null, this, {
+    isStatic: true
+  });
+  leftStopper.setScale(1); //leftFlipper.applyForce({x:3,y:0}, {x:0, y:-100})
 }
 
 function update() {
--- src/js/game.js
+++ src/js/game.js
@@ -16,7 +16,8 @@ const config = {
 }
 
 //Declare variables here
-let balls, spacebar, ball, bounds
+let balls, spacebar, ball, bounds, leftFlipper, rightFlipper, leftBumper, rightBumper,
+leftBlock, leftPivot, leftJoint, constraint1, constraint2, leftStopper
 
 const game = new Phaser.Game(config)
 
@@ -26,34 +27,89 @@ function preload() {
 }
 
 function create() {
-
     bounds = this.matter.world.setBounds(0, 0, 440, 800, 30, true, true, true, true)
     this.matter.world.setGravity(0,1)
-   
+    spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
     
     
-
+    
+    
     class Ball extends Phaser.Physics.Matter.Image {
         constructor(scene, x, y, texture) {
             super(scene.matter.world, x, y, texture)
             super.setScale(.3)
             super.setCircle(13)
             scene.sys.displayList.add(this)
-            console.log(scene)
         }
         launch(){
             super.setVelocityY(-30)
         }
     }
     
-    ball = new Ball(this, 430, 800, 'ball')
+    class Bumper extends Phaser.Physics.Matter.Sprite {
+        constructor(scene, x, y, texture){
+            super(scene.matter.world, x, y, texture)
+            super.setStatic(true)
+            scene.sys.displayList.add(this)
+        }
+    }
 
+    class Flipper extends Phaser.Physics.Matter.Image {
+        constructor(scene, x, y, texture, orientation) {
+            super(scene.matter.world, x, y, texture)
+           // super.setStatic(true)
+            super.setFriction(.2)
+            scene.sys.displayList.add(this)
+            this.setScale(0.5, 1)
+            this.rotation = .4    
+        }
+    }
+    
+    ball = new Ball(this, 215, 0, 'ball')
+
+    //Flipper mechanism
+    leftFlipper = new Flipper(this, 200, 700, 'rectA').setInteractive()
+    
+    
+    leftBlock = this.matter.add.image(175,740,'rectA', this, {
+        isStatic: true
+    })
+
+    Object.assign(leftBlock, {
+        scaleX: .5,
+        scaleY: .1,
+        rotation: .4
+    })
+
+    leftPivot = this.matter.add.image(120, 655, null, this, {
+        isStatic: true
+    })
+
+    
+
+    leftPivot.setScale(.2)
+
+    constraint1 = this.matter.add.constraint(leftPivot, leftFlipper)
+    constraint1.stiffness = .7 
+    constraint1.length = 0
+    constraint1.pointA = {
+        x: 10, 
+        y: 10
+    }
+    constraint1.pointB = {
+        x: -65, 
+        y: -40
+    }
+
+    leftStopper = this.matter.add.image(220, 600, null, this, {isStatic: true})
+    leftStopper.setScale(1)
+
+ 
+    //leftFlipper.applyForce({x:3,y:0}, {x:0, y:-100})
+
+
+    
    
-    
-
-    spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
-
-    
 }
 
 function update() {
